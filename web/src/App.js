1d3cc757ab3170b0104f72995cb587b6b9c74c7b
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-01-15 08:34:38 -0300

    feat: implementação da versão web

---
+++ web/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/DevForm/index.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+
+import "./styles.css";
+
+export default function DevForm({ onSubmit }) {
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await onSubmit({ github_username, techs, latitude, longitude });
+
+    setGithubUsername("");
+    setTechs("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do Github</label>
+        <input
+          name="github_username"
+          id="github_username"
+          value={github_username}
+          onChange={e => setGithubUsername(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          name="techs"
+          id="techs"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            name="latitude"
+            type="number"
+            id="latitude"
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            name="longitude"
+            id="longitude"
+            type="number"
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+            required
+          />
+        </div>
+      </div>
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
