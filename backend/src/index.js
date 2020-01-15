8bab61a8e6b8447c4cf08161e56fba173dc36428
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-01-15 08:49:54 -0300

    feat: implementacao do cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 mongoose.connect(
@@ -11,6 +12,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
